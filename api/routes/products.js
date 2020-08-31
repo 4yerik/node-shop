7a1ba47134093d49a9f634a506c3023cc9f6b65f
Yeri <4yerik@gmail.com>
2020-08-30 23:19:18 -0400

    mongo db

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 
 router.get('/', (req, res, next) => {
+    
     res.status(200).json({
         message: 'Handling get requests to /products'
     });
 });
 
 router.post('/', (req, res, next) => {
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // };
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body,price
+    });
+
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+
     res.status(201).json({
+        createdProduct: product,
         message: 'Handling post requests to /products'
     });
 });
