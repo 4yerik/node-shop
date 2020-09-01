1c27f4065d34ac2de13055b78eb902ef13720cd0
Yeri <4yerik@gmail.com>
2020-09-01 09:43:48 -0400

    response changes

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,10 +7,25 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+            // console.log(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
             
         })
         .catch(err => {
@@ -18,11 +33,7 @@ router.get('/', (req, res, next) => {
             res.status(500).json({
                 error: err
             })
-        })
-    
-    // res.status(200).json({
-    //     message: 'Handling get requests to /products'
-    // });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -37,8 +48,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: "handling POST requests to /products",
-                createdProduct: result
+                message: "created product",
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    requests: {
+                        type: 'GET',
+                        url: "http://localhost:3000/products/" + result._id
+                    }
+                }
             })
         })
         .catch(err => {
@@ -57,24 +76,22 @@ router.post('/', (req, res, next) => {
 
 router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    // if (id === 'hamster') {
-    //     res.status(200).json({
-    //         message: 'Yes hamsters are the best',
-    //         id: id
-    //     });
-    // } else {
-    //     res.status(200).json({
-    //         message: 'You passed an ID'
-    //     });
-    // }
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             // console.log(doc);
             console.log("from mongo", doc);
 
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        description: 'get all products',
+                        url: 'http://localhost:3000/products'
+                    }
+                });
             } else {
                 res.status(404).json({message: "no valid entry for id"});
             }
@@ -96,9 +113,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
-            console.log(result);
+            // console.log(result);
             res.status(200).json({
-                result
+                message: 'product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
             })
         })
         .catch(err => {
@@ -115,7 +136,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({_id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products',
+                    body: { name: 'String', price: 'Number' }
+                }
+            });
         })
         .catch(err => {
             console.log(err);
