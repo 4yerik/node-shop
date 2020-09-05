68daa82668e7f78e9e0c103544959294d624a70e
Yeri <4yerik@gmail.com>
2020-09-05 16:06:43 -0400

    uploading images

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Order
         .find()
         .select('product quantity _id')
+        .populate('product', 'name')
         .find()
         .exec()
         .then(docs => {
@@ -35,14 +37,22 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId,
-        quantity: req.body.quantity,
-        product: req.body.product
-    });
-
-    order
-        .save()
+    Product.findById(req.body.productId)
+        .then(product => {
+            if(!product) {
+                return res.status(404).json({
+                    message: 'product not found'
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            });
+        
+            return order
+                .save()
+        })
         .then(result => {
             console.log(result);
             res.status(201).json({
@@ -59,26 +69,61 @@ router.post('/', (req, res, next) => {
             });
         })
         .catch(err => {
-            console.log(err);
             res.status(500).json({
+                message: 'product not found',
                 error: err
-            })
+            });
         });
+    
 });
 
 
 router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'order details',
-        orderId: req.params.orderId
-    });
+    Order.findById(req.params.orderId)
+        .populate('product')
+        .exec()
+        .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: 'order not found'
+                });
+            }
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders'
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'order deleted',
-        orderId: req.params.orderId
-    });
+    Order.remove({ _id: req.params.orderId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'order deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: {
+                        productId: 'ID',
+                        quantity: "Number"
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || 
+        file,mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({ 
+    storage: storage,
+    limits: {
+        fileSize: 1024*1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
 
 const Product = require('../models/product');
 
 
 router.get('/', (req, res, next) => {
     Product.find()
-        .select('name price _id')
+        .select('name price _id productImage')
         .exec()
         .then(docs => {
             // console.log(docs);
@@ -17,6 +46,7 @@ router.get('/', (req, res, next) => {
                     return {
                         name: doc.name,
                         price: doc.price,
+                        productImage: doc.productImage,
                         _id: doc._id,
                         request: {
                             type: 'GET',
@@ -36,11 +66,13 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
+    console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
 
     product
@@ -77,7 +109,7 @@ router.post('/', (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-        .select('name price _id')
+        .select('name price _id productImage')
         .exec()
         .then(doc => {
             // console.log(doc);
