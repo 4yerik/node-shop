43b314534a1cfd33477bb2c0b5e7c831ceca7a83
Yeri <4yerik@gmail.com>
2020-09-07 20:59:26 -0400

    saving files publicly

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,10 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, './uploads/')
+        cb(null, 'uploads/')
     },
     filename: function(req, file, cb) {
+        // cb(null, file.filename);
         cb(null, new Date().toISOString() + file.originalname);
     }
 });
@@ -24,10 +25,10 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ 
     storage: storage,
-    limits: {
-        fileSize: 1024*1024 * 5
-    },
-    fileFilter: fileFilter
+    // limits: {
+    //     fileSize: 1024*1024 * 5
+    // },
+    // fileFilter: fileFilter
 });
 
 
